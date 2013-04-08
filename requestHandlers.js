f06987886f1d044576eebc96444f2df3922a9ab0
bulain <bulain@gmail.com>
2013-04-08 15:34:08 +0800

    change to use module formidable

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,9 +1,9 @@
-var querystring = require("querystring"),
-    fs = require("fs"),
-    formidable = require("formidable");
+var querystring = require("querystring");
+var fs = require("fs");
+var formidable = require("formidable");
 
 function start(request, response) {
-    console.log("Request handler 'start' was called.");
+  console.log("Request handler 'start' was called.");
 
   var body = '<html>'+
     '<head>'+
@@ -17,52 +17,52 @@ function start(request, response) {
     '</body>'+
     '</html>';
 
-    response.writeHead(200, {
-        "Content-Type" : "text/html"
-    });
-    response.write(body);
-    response.end(); 
+  response.writeHead(200, {
+    "Content-Type" : "text/html"
+  });
+  response.write(body);
+  response.end();
 
 }
 
 function upload(request, response) {
-    console.log("Request handler 'upload' was called.");
+  console.log("Request handler 'upload' was called.");
 
-    var form = new formidable.IncomingForm();
-    console.log("about to parse");
-    form.parse(request, function(error, fields, files) {
-        console.log("parsing done");
-        fs.renameSync(files.upload.path, "/tmp/test.png");
-        response.writeHead(200, {
-            "Content-Type" : "text/html"
-        });
-        response.write("received image:<br/>");
-        response.write("<img src='/show' />");
-        response.end();
+  var form = new formidable.IncomingForm();
+  console.log("about to parse");
+  form.parse(request, function(error, fields, files) {
+    console.log("parsing done");
+    fs.renameSync(files.upload.path, "/tmp/test.png");
+    response.writeHead(200, {
+      "Content-Type" : "text/html"
     });
+    response.write("received image:<br/>");
+    response.write("<img src='/show' />");
+    response.end();
+  });
 }
 
 function show(request, response) {
-    console.log("Request handler 'show' was called.");
-    fs.readFile("/tmp/test.png", "binary", function(error, file) {
-        if (error) {
-            response.writeHead(500, {
-                "Content-Type" : "text/plain"
-            });
-            response.write(error + "\n");
-            response.end();
-        } else {
-            response.writeHead(200, {
-                "Content-Type" : "image/png"
-            });
-            response.write(file, "binary");
-            response.end();
-        }
-    });
+  console.log("Request handler 'show' was called.");
+  fs.readFile("/tmp/test.png", "binary", function(error, file) {
+    if (error) {
+      response.writeHead(500, {
+        "Content-Type" : "text/plain"
+      });
+      response.write(error + "\n");
+      response.end();
+    } else {
+      response.writeHead(200, {
+        "Content-Type" : "image/png"
+      });
+      response.write(file, "binary");
+      response.end();
+    }
+  });
 }
 
 function newn(request, response) {
-    console.log("Request handler 'start' was called.");
+  console.log("Request handler 'start' was called.");
 
   var body = '<html>'+
     '<head>'+
@@ -77,31 +77,31 @@ function newn(request, response) {
     '</body>'+
     '</html>';
 
-    response.writeHead(200, {
-        "Content-Type" : "text/html"
-    });
-    response.write(body);
-    response.end(); 
+  response.writeHead(200, {
+    "Content-Type" : "text/html"
+  });
+  response.write(body);
+  response.end();
 }
 
 function create(request, response) {
-	console.log("Request handler 'create' was called.");
+  console.log("Request handler 'create' was called.");
+  var form = new formidable.IncomingForm();
+  form.parse(request, function(error, fields, files) {
+    response.writeHead(200, {
+      "Content-Type" : "text/plain"
+    });
+    response.write("You've sent the text: " + fields.text);
+    response.end();
+  });
+}
 
-	request.setEncoding("utf8");
-	var postData = "";
-
-	request.addListener("data", function(postDataChunk) {
-		postData += postDataChunk;
-		console.log("Received POST data chunk '" + postDataChunk + "'.");
-	});
-
-	request.addListener("end", function() {
-		response.writeHead(200, {
-			"Content-Type" : "text/plain"
-		});
-		response.write("You've sent the text: " + querystring.parse(postData).text);
-		response.end();
-	});
+function favicon(request, response) {
+  console.log("Request handler 'favicon' was called.");
+  response.writeHead(200, {
+    "Content-Type" : "text/plain"
+  });
+  response.end();
 }
 
 exports.start = start;
@@ -109,4 +109,5 @@ exports.upload = upload;
 exports.show = show;
 exports.newn = newn;
 exports.create = create;
+exports.favicon = favicon;
 
