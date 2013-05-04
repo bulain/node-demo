240442978a7d4558eef8a61ca839c9534c3371ff
Bulain peng <bulain@gmail.com>
2013-05-04 18:21:05 +0800

    add README

--- first-demo/server.js
+++ first-demo/server.js
@@ -9,7 +9,7 @@ function start(route, handle) {
   }
 
   http.createServer(onRequest).listen(8888);
-  console.log("Server has started.");
+  console.log("Server has started. listen port 8888.");
 }
 
 exports.start = start;
