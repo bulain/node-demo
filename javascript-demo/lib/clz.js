da664cc07dd958968746e3006fb9567b2a59c90c
bulain <bulain@gmail.com>
2013-05-13 09:46:56 +0800

    initial javascript-demo

---
+++ javascript-demo/lib/clz.js
@@ -0,0 +1,11 @@
+define(function() {
+  var env = {
+    constructor : function() {
+    },
+    _options : {},
+    initialize : function(options, callback) {
+      this._options = options;
+    }
+  }
+  return env;
+});
