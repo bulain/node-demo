634d0e53564e184d54a4f9a2e70375dacee4d9f5
Bulain Peng <bulain@gmail.com>
2018-06-24 10:43:35 +0800

    update mongo methods

--- oapi-demo/lib/mongo.js
+++ oapi-demo/lib/mongo.js
@@ -4,49 +4,55 @@ const ObjectId = require('mongodb').ObjectId;
 
 const url = 'mongodb://localhost:27017';
 const dbname = 'oapi';
-const db = function () {
-    if (global.mongo) return;
-    return MongoClient.connect(url, {
-        native_parser: false,
-        poolSize: 50,
-        noDelay: false,
-        autoReconnect: true
-    }).then(function (conn) {
-        global.mongo = conn.db(dbname);
-    }).catch(function (e) {
-        console.log(e);
-    });
+
+let database = async function () {
+    if (global._db) return global._db;
+    try {
+        let client = await MongoClient.connect(url, {
+            native_parser: false,
+            poolSize: 50,
+            noDelay: false,
+            autoReconnect: true
+        });
+        global._db = client.db(dbname);
+    } catch (err) {
+        console.log(err);
+    }
+    return global._db;
 };
 
-const save = async (name, obj) => {
-    await db();
+// 初始化数据库连接
+database();
+
+let save = async (name, obj) => {
+    let db = await database();
     if (obj._id) obj._id = ObjectId(obj._id);
-    return global.mongo.collection(name).save(obj);
+    return db.collection(name).save(obj);
 };
 
-const insertOrUpdate = async (name, obj) => {
-    await db();
+let insertOrUpdate = async (name, obj) => {
+    let db = await database();
     if (obj._id) {
         obj._id = ObjectId(obj._id);
-        return global.mongo.collection(name).updateOne({ "_id": obj._id }, { $set: obj });
+        return db.collection(name).updateOne({ "_id": obj._id }, { $set: obj });
     } else {
-        return global.mongo.collection(name).insertOne(obj);
+        return db.collection(name).insertOne(obj);
     }
 };
 
-const find = async (name, query, opt) => {
-    await db();
-    var q = global.mongo.collection(name).find(query);
+let find = async (name, query, opt) => {
+    let db = await database();
+    var q = db.collection(name).find(query);
     if (opt.skip) q = q.skip(opt.skip);
     if (opt.limit) q = q.limit(opt.limit);
     if (opt.sort) q = q.sort(opt.sort);
     return q.toArray();
 };
 
-const remove = async (name, obj) => {
-    await db();
+let remove = async (name, obj) => {
+    let db = await database();
     if (obj._id) obj._id = ObjectId(obj._id);
-    return global.mongo.collection(name).deleteOne(obj);
+    return db.collection(name).deleteOne(obj);
 };
 
 //模块导出
