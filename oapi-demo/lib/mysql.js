5bc1573e0a3db9fc73f567e5d7f8d6ebb2bc78f7
Bulain Peng <bulain@gmail.com>
2018-06-24 18:15:20 +0800

    refactor mysql demo

--- oapi-demo/lib/mysql.js
+++ oapi-demo/lib/mysql.js
@@ -1,5 +1,8 @@
 'use strict';
 const mysql = require('mysql');
+const bluebird = require("bluebird");
+bluebird.promisifyAll(require("mysql/lib/Connection").prototype);
+bluebird.promisifyAll(require("mysql/lib/Pool").prototype);
 
 var pool = null;
 
@@ -25,84 +28,35 @@ var shutdown = () => {
 };
 
 //将结果已对象数组返回
-var row = (sql, ...params) => {
-    return new Promise(function (resolve, reject) {
-        pool.getConnection(function (err, connection) {
-            if (err) {
-                reject(err);
-                return;
-            }
-            connection.query(sql, params, function (error, res) {
-                connection.release();
-                if (error) {
-                    reject(error);
-                    return;
-                }
-                resolve(res);
-            });
-        });
-    });
+var row = async (sql, ...params) => {
+    var conn = await pool.getConnectionAsync();
+    var res = await conn.queryAsync(sql, params);
+    conn.release();
+    return res;
 };
 //返回一个对象
-var first = (sql, ...params) => {
-    return new Promise(function (resolve, reject) {
-        pool.getConnection(function (err, connection) {
-            if (err) {
-                reject(err);
-                return;
-            }
-            connection.query(sql, params, function (error, res) {
-                connection.release();
-                if (error) {
-                    reject(error);
-                    return;
-                }
-                resolve(res[0] || null);
-            });
-        });
-    });
+var first = async (sql, ...params) => {
+    var conn = await pool.getConnectionAsync();
+    var res = await conn.queryAsync(sql, params);
+    conn.release();
+    return res[0] || null;
 };
 //返回单个查询结果
-var single = (sql, ...params) => {
-    return new Promise(function (resolve, reject) {
-        pool.getConnection(function (err, connection) {
-            if (err) {
-                reject(err);
-                return;
-            }
-            connection.query(sql, params, function (error, res) {
-                connection.release();
-                if (error) {
-                    reject(error);
-                    return;
-                }
-                for (let i in res[0]) {
-                    resolve(res[0][i] || null);
-                    return;
-                }
-                resolve(null);
-            });
-        });
-    });
+var single = async (sql, ...params) => {
+    var conn = await pool.getConnectionAsync();
+    var res = await conn.queryAsync(sql, params);
+    conn.release();
+    for (let i in res[0]) {
+        return res[0][i] || null;
+    }
+    return null;
 }
 //执行代码，返回执行结果
-var execute = (sql, ...params) => {
-    return new Promise(function (resolve, reject) {
-        pool.getConnection(function (err, connection) {
-            if (err) {
-                reject(err);
-                return;
-            }
-            connection.query(sql, params, function (error, res) {
-                connection.release();
-                if (error) {
-                    reject(error);
-                    return;
-                }
-                resolve(res);
-            });
-        });
-    });
+var execute = async (sql, ...params) => {
+    var conn = await pool.getConnectionAsync();
+    var res = await conn.queryAsync(sql, params);
+    conn.release();
+    return res;
 }
 
 //模块导出
