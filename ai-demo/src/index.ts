9bff5c6c2f4ec23775dfd261e9ce3a9ac8c23381
Bulain Peng <bulain@gmail.com>
2025-05-20 13:07:55 +0800

    update ai demo

---
+++ ai-demo/src/index.ts
@@ -0,0 +1,86 @@
+import express from "express";
+import { Request, Response } from "express";
+import { randomUUID } from "node:crypto";
+import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
+import { StreamableHTTPServerTransport } from "@modelcontextprotocol/sdk/server/streamableHttp.js";
+import { isInitializeRequest } from "@modelcontextprotocol/sdk/types.js"
+import { z } from "zod";
+
+const getServer = () => {
+    const server = new McpServer({
+          name: "MCP Server",
+          version: "1.0.0"
+        });
+
+    server.tool(
+      "echo",
+      { message: z.string() },
+      async ({ message }) => ({
+        content: [{ type: "text", text: `Tool echo: ${message}` }]
+      })
+    );
+
+    return server;
+
+    }
+
+const app = express();
+app.use(express.json());
+app.post('/mcp', async (req: Request, res: Response) => {
+  try {
+    const server = getServer(); 
+    const transport: StreamableHTTPServerTransport = new StreamableHTTPServerTransport({
+      sessionIdGenerator: undefined,
+    });
+    res.on('close', () => {
+      console.log('Request closed');
+      transport.close();
+      server.close();
+    });
+    await server.connect(transport);
+    await transport.handleRequest(req, res, req.body);
+  } catch (error) {
+    console.error('Error handling MCP request:', error);
+    if (!res.headersSent) {
+      res.status(500).json({
+        jsonrpc: '2.0',
+        error: {
+          code: -32603,
+          message: 'Internal server error',
+        },
+        id: null,
+      });
+    }
+  }
+});
+
+app.get('/mcp', async (req: Request, res: Response) => {
+  console.log('Received GET MCP request');
+  res.writeHead(405).end(JSON.stringify({
+    jsonrpc: "2.0",
+    error: {
+      code: -32000,
+      message: "Method not allowed."
+    },
+    id: null
+  }));
+});
+
+app.delete('/mcp', async (req: Request, res: Response) => {
+  console.log('Received DELETE MCP request');
+  res.writeHead(405).end(JSON.stringify({
+    jsonrpc: "2.0",
+    error: {
+      code: -32000,
+      message: "Method not allowed."
+    },
+    id: null
+  }));
+});
+
+
+// Start the server
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`MCP Stateless Streamable HTTP Server listening on port ${PORT}`);
+});
